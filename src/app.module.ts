7235b77cce7953b1b4201506e0abc6ae91337ed1
Vune <nguyenvu2319@gmail.com>
2023-12-07 04:32:58 +0700

    feat: config import MongooseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AccountModule } from './account/account.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `${process.env.DB_CONNECTION}/${process.env.DB_NAME}`,
+    ),
+    AccountModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

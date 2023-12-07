e46a8c5e8b1959264c140262cf2d9b25299ddf1b
Vune <nguyenvu2319@gmail.com>
2023-12-08 02:37:26 +0700

    feat: create modules and reconfig account module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AccountModule } from './account/account.module';
+import { AccountModule } from './modules/account/account.module';
+import { UserModule } from './modules/user/user.module';
+import { BlogModule } from './modules/blog/blog.module';
+import { StyleModule } from './modules/style/style.module';
+import { LayoutModule } from './modules/layout/layout.module';
+import { PublicPageModule } from './modules/public-page/public-page.module';
+import { StarModule } from './modules/actions/star/star.module';
+import { LikeModule } from './modules/actions/like/like.module';
+import { ShareModule } from './modules/actions/share/share.module';
+import { CommentModule } from './modules/actions/comment/comment.module';
+import { FavoriteModule } from './modules/favorite/favorite.module';
 
 @Module({
   imports: [
@@ -12,6 +22,16 @@ import { AccountModule } from './account/account.module';
       `${process.env.DB_CONNECTION}/${process.env.DB_NAME}`,
     ),
     AccountModule,
+    UserModule,
+    BlogModule,
+    StyleModule,
+    LayoutModule,
+    PublicPageModule,
+    StarModule,
+    LikeModule,
+    ShareModule,
+    CommentModule,
+    FavoriteModule,
   ],
   controllers: [AppController],
   providers: [AppService],

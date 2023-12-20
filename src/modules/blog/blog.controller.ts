3da49f01827b84bbf424fd439ed0e5afe1d7acbd
Vune <nguyenvu2319@gmail.com>
2023-12-21 01:14:17 +0700

    feat: upload file blog images

--- src/modules/blog/blog.controller.ts
+++ src/modules/blog/blog.controller.ts
@@ -1,6 +1,18 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { BlogService } from './blog.service';
 import { Blog } from './schemas/blog.schema';
+import { storageConfig } from 'helpers/config';
 
 @Controller('api/blogs')
 export class BlogController {
@@ -26,8 +38,26 @@ export class BlogController {
     return await this.blogService.create(blog);
   }
 
+  @Post('upload-file')
+  @UseInterceptors(
+    FileInterceptor('file', { storage: storageConfig('images/blogs') }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+    return JSON.stringify(file.destination + '/' + file.filename);
+  }
+
+  @Post('upload-files')
+  @UseInterceptors(
+    FilesInterceptor('files', 50, { storage: storageConfig('images/blogs') }),
+  )
+  uploadFiles(@UploadedFiles() files: Array<Express.Multer.File>) {
+    console.log(files);
+    return JSON.stringify(files);
+  }
+
   @Put('update/:id')
   async update(@Param('id') user_id: string): Promise<Blog> {
-    return await this.blogService.findBy('user_id', user_id); // ...
+    return await this.blogService.findBy('user_id', user_id);
   }
 }
